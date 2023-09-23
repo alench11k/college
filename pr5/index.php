a9b430db8a372cad354e4f7ab1a4bc42b65c1e8b
alench11k <144382313+alench11k@users.noreply.github.com>
2023-09-23 10:32:11 +0500

    Готова 5 практика

--- pr5/index.php
+++ pr5/index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+    
+
 <?php
     $host       = "db4.myarena.ru";      // Адрес сервера базы данных
     $dbname     = "u19978_b03";          // Имя базы данных
@@ -6,16 +14,32 @@
    
     try{
         $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
-        echo ('Подключение успешно');
+       // echo ('Подключение успешно');
     }
         catch(PDOException $ex){
         die('Произошла ошибка');
     }
-    $result = $connection->query('SELECT * FROM `product`');
-        while($row = $result->fetch( PDO::FETCH_ASSOC )){
-        foreach ($row as $key => $value) {
+    
+$result = $connection->query('SELECT * FROM product');
+echo "<table class = 'table' border = '1'>";
+
+$row = $result->fetch( PDO::FETCH_ASSOC );
+    echo "<tr>";
+    foreach ($row as $key => $value) {
+        echo "<th>".$key."</th>";
+    };
+    echo "</tr>";
+{
+do {
+    echo "<tr>";
+    foreach ($row as $key => $value) {
         echo "<td>".$value."</td>";
     }
+    echo "</tr>";
+} while($row = $result->fetch( PDO::FETCH_ASSOC )); 
 }
+echo "</table>";
 
 ?>
+</body>
+</html>
