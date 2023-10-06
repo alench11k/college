fc1bc85298179674adcde6e02e0f8ca816606012
alench11k <144382313+alench11k@users.noreply.github.com>
2023-10-06 11:01:31 +0500

    Подключение к базе данных

--- pr7/action.php
+++ pr7/action.php
@@ -1,16 +1,43 @@
 <?php 
+    $host       = "db4.myarena.ru";      // Адрес сервера базы данных
+    $dbname     = "u19978_b03";          // Имя базы данных
+    $user       = "u19978_b03";          // Имя пользователя
+    $password   = "iH3tS3bV4w";         // Пароль
+
+try{
+    $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
+   // echo ('Подключение успешно');
+   
+}
+    catch(PDOException $ex){
+    die('Произошла ошибка');
+}
+
+$result = $connection->query('SELECT * FROM `users`');
     if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit ("Что-то пошло не так...");
         if(empty($_POST['login'])) exit('Поле "Логин" не заполнено');
+
         if(empty($_POST['password1'])) exit('Поле "Пароль" не заполнено');
+
         if( strlen ($_POST['password1']) < 8) exit('В поле "Пароль" введено мало символов');
+
         if( strlen ($_POST['password1']) > 15) exit('В поле "Пароль" введено много символов');
+
         if ($_POST['password1']!==$_POST['password2']) {
            exit ('Пароли не совпадают');
         }
         if(empty($_POST['mail'])) exit('Поле "Почта" не заполнено');
-        
-        
 
-        echo 'Поздравляем с успешной регистрацией, '.htmlspecialchars($_POST['login']).'. <br>';
-    
-?>
+        
+        $data = [$_POST['login'], $_POST['password1'], $_POST['mail']];
+        $insert = $connection-> prepare ("INSERT INTO `users`(`login`,`password`,`mail`) VALUES (?, ?, ?);");
+        $result = $insert-> execute ($data);
+
+        if($result){
+            echo ('Поздравляем с успешной регистрацией, ');
+        }
+        else {
+            exit ('Ошибка в процессе регистрации.');
+        }
+        
+?>   
--- pr7/index.php
+++ pr7/index.php
@@ -6,7 +6,6 @@
     <title>Практика 7</title>
 </head>
 <body>
-
     <form action="action.php" method="post">
     <p>Введите логин*: <input type="text" name="login" required name = "login" placeholder = "Введите логин"></p>
     <p>Придумайте пароль (не более 15 символов и не менее 8)*: <input type="password" name="password1" required name = "password1" min=1 placeholder = "Введите пароль"></p>
@@ -14,6 +13,5 @@
     <p>Введите адрес электронной почты*: <input type="email" name="mail" required name = "mail" min=1 placeholder = "Введите почту"></p>
     <p><input type="submit"></p> 
     </form>
-
 </body>
 </html>
