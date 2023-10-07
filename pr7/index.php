d6205d530933bbffb3495bc51cace7fe86347dff
alench11k <144382313+alench11k@users.noreply.github.com>
2023-10-07 11:08:19 +0500

    Update index.php

--- pr7/index.php
+++ pr7/index.php
@@ -8,13 +8,7 @@
 <body>
     <form action="action.php" method="post">
     <p>Введите логин*: <input type="text" name="login" required name = "login" placeholder = "Введите логин"></p>
-    <p>Придумайте пароль (не более 15 символов и не менее 8)*: <input type="password" name="password1" required name = "password1" min=1 placeholder = "Введите пароль">
-    <?php
-	$login = $_POST['login'];
-	$password = md5($_POST['password']); // преобразуем пароль в его хеш
-	
-	$query = "INSERT INTO users SET login='$login', password='$password'";
-?></p>
+    <p>Придумайте пароль (не более 15 символов и не менее 8)*: <input type="password" name="password1" required name = "password1" min=1 placeholder = "Введите пароль"></p>
     <p>Повторите пароль*: <input type="password" name="password2" required name = "password2" min=1 placeholder = "Повторите пароль"></p>
     <p>Введите адрес электронной почты*: <input type="email" name="mail" required name = "mail" min=1 placeholder = "Введите почту"></p>
     <p><input type="submit"></p> 
